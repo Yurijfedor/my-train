38c5083616d71849b2653943631d362bafe1a071
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-06-04 17:12:17 +0300

    add slider

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: "ease",
+    infinite: true,
+    initialSlide: 2,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: false,
+    centerMode: false,
+    variableWidth: false,
+    rows: 1,
+    slildesPerRow: 1,
+    vertical: false,
+  });
+});
